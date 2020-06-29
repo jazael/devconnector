d4ce4b4278daa3c65ff6d991f8941db518c50763
Miguel-Faubla <fanthonymiguel@hotmail.com>
2020-06-29 00:36:17 -0500

    Routes, Models, Validation, Passport JWT, API[Register, Login, CurrentUser]

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: 'mongodb://jazael:12345J@ds125262.mlab.com:25262/devconnector',
+    secretOrKey: 'secret'
+}
--- server.js
+++ server.js
@@ -1,7 +1,34 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello word any any any'));
+// Middleware Parser Body
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const db = require('./config/keys').mongoURI;
+mongoose
+    .connect(db)
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
+
+// Middleware Passport
+app.use(passport.initialize());
+require('./config/passport')(passport);
+
+app.get('/', (req, res) => res.send('Hello word'));
+
+// Routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
 const port = process.env.PORT || 5000;
 
