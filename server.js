870291ebdd625fd968bcb386de90b91aeaebe656
Miguel-Faubla <fanthonymiguel@hotmail.com>
2020-06-28 19:59:25 -0500

    Initial configuration API

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.send('Hello word any any any'));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
