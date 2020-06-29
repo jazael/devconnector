533afd8945ab1045813786df340394fd993f28f2
Miguel Faubla <fanthonymiguel@hotmail.com>
2020-06-29 01:11:23 -0500

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb://jazael:12345J@ds125262.mlab.com:25262/devconnector',
+    mongoURI: 'mongodb://user:passJ@db_id.mlab.com:port/devconnector',
     secretOrKey: 'secret'
 }
